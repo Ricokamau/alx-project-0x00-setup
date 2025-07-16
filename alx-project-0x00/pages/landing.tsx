6a37e1d3577871c40a33b00805de254fd5a88dc4
Ricokamau <ricokamau@gmail.com>
2025-07-16 13:02:48 +0300

    Create button components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,16 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button";
+
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Small" styles="text-sm rounded-sm" />
+<Button title="Medium" styles="text-base rounded-md" />
+<Button title="Large" styles="text-lg rounded-full" />
+
     </div>
   );
 };
